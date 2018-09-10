da66f93c6c94102212d8d5103ed6336ed3838bb6
david m flynn <flynndave1@msn.com>
2018-09-10 23:02:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 /* Enter the code to remove the main node element under this comment */
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+document.querySelector('main').remove();
+var element = document.createElement('div');
+
+document.body.appendChild(element);
+
+var ul = document.createElement('ul');
+ 
+for (let i = 0; i < 7; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+ element.appendChild(ul);
+ 
+ var element2 = document.createElement('h1');
+ document.body.appendChild(element2);
+
+ element2.innerHTML = 'Hello, DOM!';
+element2.style.backgroundColor = '#ff0090';
+element2.style.textAlign = 'center';
+ul.style.textAlign = 'left';
+
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
+var newHeader = document.createElement("h1");
+
+document.body.appendChild(newHeader);
+newHeader.setAttribute("id", "victory");
+ newHeader.innerHTML = 'David is the Champion!!';
+ newHeader.style.backgroundColor = '#00ff90';
+ 
+
+
+
+
